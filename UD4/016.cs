bea97df955b88e04eeb441df8bc8b0320053290f
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 12:53:15 +0100

    solucion

---
+++ UD4/016.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _016
+    {
+        static void Main(string[] args)
+        {
+            double precio,iva=0.21, precioFinal;
+
+             Console.WriteLine("Introduce el numero");
+             precio = Convert.ToDouble(Console.ReadLine());
+
+            precioFinal = precio + (precio * iva);
+            Console.WriteLine(precio);
+
+        }
+    }
+}
