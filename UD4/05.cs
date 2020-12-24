d6210f3176bd5754b351b54f3dab0ca60067405c
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 11:00:53 +0100

    solucion

---
+++ UD4/05.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _05
+    {
+        static void Main(string[] args)
+        {
+            string diadesemana;
+
+            Console.WriteLine("Escribe que dia de la semana es");
+            diadesemana = Console.ReadLine();
+
+            switch (diadesemana.ToLower())
+            {
+
+                case "lunes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "martes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "miercoles":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "jueves":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "viernes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "sabado":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                case "domingo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+
+                default:
+                    Console.WriteLine("Ese dia no es correcto");
+                    break;
+            }
+
+
+            Console.ReadLine();
+        }
+    }  
+}
