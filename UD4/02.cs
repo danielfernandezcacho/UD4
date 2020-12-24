65acaa8d6c5efd4462403fe2b57dfe8799dbf2ba
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 10:12:31 +0100

    solucion

---
+++ UD4/02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _02
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String ciudad;
+
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu ciudad");
+            ciudad = Console.ReadLine();
+
+            Console.WriteLine("hola "+ nombre +" bienbenido a la ciudad de "+ ciudad);
+
+        }
+    }
+}
