5073b03f5c55a16ba4cbddf10638d820e4c540ad
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 12:43:52 +0100

    solucion

---
+++ UD4/015.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _015
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Introduce un numero");
+            int numero = Int32.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero " + numero + " es divisible entre 2");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + numero + " no es divisible entre 2");
+            }
+        }
+    }
+}
