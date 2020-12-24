9f71a8a0140f5e91c50920e4cdddcd86cddf810c
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 10:31:23 +0100

    solucion

---
+++ UD4/03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _03
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String años;
+            int anyos=0;
+
+
+            Console.WriteLine("Escribe tu edat");
+            años = Console.ReadLine();
+            anyos = Convert.ToInt32(años);
+
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("hola " + nombre + " tu edat es " + años);
+        }
+    }
+}
