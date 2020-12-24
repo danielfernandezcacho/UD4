339621bfa14c29da0bc2a7444371a014ef24d177
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 12:11:10 +0100

    solucion

---
+++ UD4/012.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _012
+    {
+        public static void main(String[] args)
+        {
+
+            String nombre = "Daniel";
+
+            Console.WriteLine("Bienvenido " + nombre);
+        }
+    }
+}
