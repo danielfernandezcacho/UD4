6f8ee618673607cc7ab1d22644276a290f109daf
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 11:59:26 +0100

    solucion

---
+++ UD4/010.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _010
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
