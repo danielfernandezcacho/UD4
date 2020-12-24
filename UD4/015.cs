c2e03cec5852b7b2d1d32ac9332a10f843efa124
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 12:25:32 +0100

    solucion

---
+++ UD4/015.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _015
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+
+            Console.WriteLine("Introduce un radio");
+            radio = Console.ReadLine();
+            double area = Math.PI * Math.pow(radio, 2);
+
+         Console.WriteLine("El area del circulo es "+area);
+    }
+}
+}
