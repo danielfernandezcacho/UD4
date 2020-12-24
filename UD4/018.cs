500d06c7e108eeefbfc17de6dd3ee663d6893d5f
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 13:00:06 +0100

    solucion

---
+++ UD4/018.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _018
+    {
+
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++) {
+               Console.WriteLine(num);
+
+            } 
+        }
+    }
+}
