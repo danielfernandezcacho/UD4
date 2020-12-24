0f1d46414e45092af94798ea5684c70d73066202
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 12:19:08 +0100

    solucion

---
+++ UD4/013.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _013
+    {
+        static void Main(string[] args)
+        {
+
+            String nombre;
+
+             Console.WriteLine("Introduce tu nombre");
+             nombre = Console.ReadLine();
+             Console.WriteLine("Bienvenido " + nombre);
+
+        }
+    }
+}
