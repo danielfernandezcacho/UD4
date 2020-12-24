b3049bc0b66d5f9d828fbbe114eb28feb4b63137
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 12:07:07 +0100

    solucion

---
+++ UD4/011.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _011
+    {
+        public static void main(String[] args)
+        {
+
+        
+            int num1 = 6;
+            int num2 = 7;
+
+            
+            if (num1 >= num2)
+            {
+                
+                if (num1 == num2)
+                {
+                    Console.WriteLine("els numeros " + num1 + " y " + num2 + " son iguals");
+                }
+                else
+                {
+                    Console.WriteLine("El número " + num1 + " es mayor que el número " + num2);
+                }
+                }
+                else
+                {
+                    Console.WriteLine("El número " + num2 + " es mayor que el número " + num1);
+                }
+        }
+    }
+}
