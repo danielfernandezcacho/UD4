7344a1b753ce2bd6db02529c2df90a700ef585ba
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 12:58:09 +0100

    solucion

---
+++ UD4/017.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _017
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            while (num <= 100)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+
+        }
+    }
+}
