eaaa644dcc80462e6fd0a79437f8a5956fd0f424
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 11:57:57 +0100

    solucion

---
+++ UD4/09.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _09
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
