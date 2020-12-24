19c2db9f6e29a70c30353fc8cb094828a1465cd6
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-24 10:02:54 +0100

    solucion

---
+++ UD4/01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD4
+{
+    class _01
+    {
+        static void Main(string[] args)
+        {
+            int a = 4, b = 3, c = 9, suma = 0;
+
+            suma = a + b + c;
+            Console.WriteLine(suma);
+        }
+    }
+}
